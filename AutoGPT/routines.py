d7e0e6c1f226d1728be023b34e4b55ff68d6c5da
marco eterno <Marcolino97do@gmail.com>
2023-11-20 16:38:27 +0100

    updated README.md with details on the memory architectures, and created the working_memory.py file

--- AutoGPT/routines.py
+++ AutoGPT/routines.py
@@ -3,7 +3,7 @@ def context_memory_management(mission,process,worldview,useful_infos='', context
     in context memory management that uses RAG from the worldview+summarization to fit all useful info in the context lenght
     """
 
-def determine_wHether_user_input_is_required():
+def determine_whether_user_input_is_required():
     """
     determine whether user input is required and returns a boolean
     """
