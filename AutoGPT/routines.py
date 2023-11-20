93f8a15a348b64785dd3377d0e7aea2b1130695a
marco eterno <Marcolino97do@gmail.com>
2023-11-20 15:11:06 +0100

    created worldview.txt and routines file

---
+++ AutoGPT/routines.py
@@ -0,0 +1,6 @@
+def context_memory_management(mission,process,worldview,useful_infos='', context_lenght=8000):
+    """
+    in context memory management that uses RAG from the worldview+summarization to fit all useful info in the context lenght
+    """
+
+def
