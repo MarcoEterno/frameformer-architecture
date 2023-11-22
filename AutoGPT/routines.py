d63040f71adae2cb2fc77700569c83cb5654d6cb
gitpaxt <paxtognini@gmail.com>
2023-11-22 12:20:23 +0100

    test.py still on progress

--- AutoGPT/routines.py
+++ AutoGPT/routines.py
@@ -7,6 +7,14 @@ import os
 # Download the punkt tokenizer (if not already downloaded)
 nltk.download('punkt')
 
+def open_file_get_text(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+            file.close()
+            return text
+    except:
+        raise Exception("Error: file not found")
 
 def count_tokens(file_path):
     try:
@@ -18,41 +26,40 @@ def count_tokens(file_path):
     except:
         raise Exception("Error: file not found")
 
+def count_tokens_of_string(text):
+    tokens = nltk.word_tokenize(str(text))
+    return len(tokens)
+
 
 """
-in context memory management that uses RAG from the worldview + summarization to fit all useful info in the context lenght
+in context memory management that uses RAG from the worldview + summarization to fit all useful info in the context length
 """
 
 
 
-def summarization_of_infos_relevant_to_task(task, world_model, default_llm, context_lenght=8000):
+def summarization_of_infos_relevant_to_task(task, text_to_summarize, default_llm, context_length=8000):
     """Summarizes useful info"""
 
     """First step: counting the context length"""
-    file_name = 'world_model.txt'
-    file_path = os.path.join(os.getcwd(),file_name)
-    num_tokens = count_tokens(file_path)
-
-    print(f'Number of tokens in {file_path}: {num_tokens}')  # then comment this line
+    num_tokens = count_tokens_of_string(text_to_summarize)
 
     """Second step: reads all the world_model and summarizes the relevant info"""
-    iterations = num_tokens // context_lenght + 1
+    iterations = num_tokens // context_length + 1
     summarization = []
 
     for i in range(iterations):
-        prompt = "YOUR TASK IS: " + task + " READ THIS WORLD MODEL WRITTEN BY YOU AND OUTPUT" + \
+        prompt = "YOUR TASK IS: " + str(task) + " READ THIS WORLD MODEL WRITTEN BY YOU AND OUTPUT" + \
                  "ALL THE RELEVANT INFORMATION TO SOLVE THE TASK. YOU ARE NOW SUMMARIZING THE " + str(i) + " / " + \
-                 str(iterations) + " PART OF THE WORLD MODEL, WHICH FOLLOWS: " + world_model
+                 str(iterations) + " PART OF THE WORLD MODEL, WHICH FOLLOWS: " + str(text_to_summarize)
         summarization.append(default_llm(prompt) + '\n')
 
     """Third step"""
     num_summarizations_done = 1
-    while count_tokens(summarization) > context_lenght and num_summarizations_done < 3:
+    while count_tokens_of_string(summarization) > context_length and num_summarizations_done < 3:
         # first argue that the summarization is too long, we define a string, and pass it to the function.
-        summarization_of_infos_relevant_to_task(task + "Also: YOU HAVE ALREADY SUMMARIZED THE INFO: BUT IT WAS TOO LONG: " + \
-                                                count_tokens(
-                                           summarization) + " INSTEAD OF THE CONTEXT LENGTH " + context_lenght + "TRY TO SUMMARIZE IT IN LESS TOKENS!", \
-                                                world_model, default_llm)
+        summarization_of_infos_relevant_to_task(str(task) + "Also: YOU HAVE ALREADY SUMMARIZED THE INFO: BUT IT WAS TOO LONG: " + \
+                                                count_tokens_of_string(str(summarization)) + " INSTEAD OF THE CONTEXT LENGTH " + str(context_length) + \
+                                                " NOW TRY TO SUMMARIZE IT IN LESS TOKENS!", text_to_summarize, default_llm, context_length)
         num_summarizations_done += 1
 
     return summarization
